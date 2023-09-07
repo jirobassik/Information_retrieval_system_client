76cd78a377e6b317403b9d20621d175d15350b79
= <jirobassik@tut.by>
2023-09-07 09:41:12 +0300

    Add check input, home link

--- search_irs/views.py
+++ search_irs/views.py
@@ -4,21 +4,33 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render
 from django.urls import reverse
 import cgi
-
+import speech_recognition as sr
 from utils.init_json_ser_req import file_request, query_request, query_json_serializer, download_request
 from utils.queryset_upl import file_queryset_upl
 
 
 def search(request):
     if 'query_button' in request.POST:
-        serialize_query = query_json_serializer.encode(query=request.POST['query'])
-        data_query_request = query_request.get_request_data(serialize_query)
-        dict_filenames = json.loads(data_query_request)
-        queryset_file = file_queryset_upl()
-        return render(request, 'search_irs/search.html', {'dict_filenames': dict_filenames, 'files': queryset_file})
-    if 'upl' in request.POST:
-        if upl_file := request.FILES.get('document', False):
-            file_request.post_request_file(upl_file)
+        return handle_query_request(request)
+    elif 'upl' in request.POST:
+        handle_upl_request(request)
+    return show_main_view(request)
+
+
+def handle_upl_request(request):
+    if upl_file := request.FILES.get('document', False):
+        file_request.post_request_file(upl_file)
+
+
+def handle_query_request(request):
+    serialize_query = query_json_serializer.encode(query=request.POST['query'])
+    data_query_request = query_request.get_request_data(serialize_query)
+    dict_filenames = json.loads(data_query_request)
+    queryset_file = file_queryset_upl()
+    return render(request, 'search_irs/search.html', {'dict_filenames': dict_filenames, 'files': queryset_file})
+
+
+def show_main_view(request):
     queryset_file = file_queryset_upl()
     return render(request, 'search_irs/main_view.html', {'files': queryset_file})
 
@@ -35,3 +47,18 @@ def download_file(request, pk):
     response = HttpResponse(file_data, content_type='application/octet-stream')
     response['Content-Disposition'] = f'attachment; filename="{filename}"'
     return response
+
+
+def speech_to_text(request):
+    recognizer = sr.Recognizer()
+    print('start micro')
+    with sr.Microphone() as source:
+        try:
+            audio_data = recognizer.listen(source)
+            text = recognizer.recognize_google(audio_data)
+
+        except sr.UnknownValueError:
+            text = "Could not understand the audio"
+        except sr.RequestError as e:
+            text = f"Speech recognition error: {str(e)}"
+    print('django text', text)
